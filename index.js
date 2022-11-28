eae50220fef0042c99aaba133f693b8256657631
Yana Kochikhina <yana.kochikhina@gmail.com>
2022-11-28 14:32:07 +0300

    made changes in main

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log('Hello,World');
-console.log('Hello')
+
+const userAge = 35;
+if (userAge >=18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы несовершеннолетний')
+}
